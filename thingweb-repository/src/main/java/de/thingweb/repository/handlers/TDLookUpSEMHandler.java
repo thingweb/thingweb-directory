3d9dce567a3745e3c548db2f6be59d6752d6a43c
Thuluva Aparna Saisree z003j0hn <aparna.thuluva@siemens.com>
2017-07-05 16:00:00 +0000

    Update TDLookUpSEMHandler.java to post complex SPARQL queries to retrieve matching graphs

--- thingweb-repository/src/main/java/de/thingweb/repository/handlers/TDLookUpSEMHandler.java
+++ thingweb-repository/src/main/java/de/thingweb/repository/handlers/TDLookUpSEMHandler.java
@@ -5,6 +5,33 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+import org.apache.jena.atlas.lib.StrUtils;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryExecution;
+import org.apache.jena.query.QueryExecutionFactory;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.query.QuerySolution;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.query.ResultSet;
+
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.NodeIterator;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.ResourceFactory;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.riot.RDFDataMgr;
+import org.apache.jena.sparql.util.QueryExecUtils;
 
 import de.thingweb.repository.ThingDescriptionUtils;
 import de.thingweb.repository.rest.BadRequestException;
@@ -13,6 +40,7 @@ import de.thingweb.repository.rest.RESTException;
 import de.thingweb.repository.rest.RESTHandler;
 import de.thingweb.repository.rest.RESTResource;
 import de.thingweb.repository.rest.RESTServerInstance;
+import de.thingweb.repository.Repository;
 
 public class TDLookUpSEMHandler extends RESTHandler {
 
@@ -116,6 +144,68 @@ public class TDLookUpSEMHandler extends RESTHandler {
 		}
 		
 		resource.content += "}";
+		return resource;
+	}
+	
+		@Override
+	public RESTResource post(URI uri, Map<String, String> parameters, InputStream payload) throws RESTException {
+		String data = "";
+		List<String> tds = new ArrayList<>();
+		try {
+			data = ThingDescriptionUtils.streamToString(payload);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			throw new BadRequestException();
+		}
+
+		String resultTriples = new String();
+		RESTResource resource = new RESTResource(uri.toString(), this);
+		resource.contentType = "application/ld+json";
+		resource.content = "{";	
+
+		try {
+			if (data.contains("graph") || !data.contains(";")) {
+
+				if (data.contains(";")){
+					data = data.substring(data.indexOf(";") + 1);
+				}
+               tds = ThingDescriptionUtils.listThingDescriptions(data);
+			  
+                // Retrieve Thing Description(s)
+				for (int i = 0; i < tds.size(); i++) {
+					URI td = URI.create(tds.get(i));
+					ThingDescriptionHandler h = new ThingDescriptionHandler(td.toString(), instances);
+					RESTResource res = h.get(td, new HashMap<String, String>());
+					// TODO check TD's content type
+
+					resource.content += "\"" + td.getPath() + "\": " + res.content;
+					if (i < tds.size() - 1) {
+						resource.content += ",";
+					}
+				}
+			} else if (data.contains("triple")) {
+				data = data.substring(data.indexOf(";")+1);
+				Dataset dataset = Repository.get().dataset;
+		        dataset.begin(ReadWrite.READ);
+				String q = "SELECT DISTINCT " + data;
+				QueryExecution qexec = QueryExecutionFactory.create(q, dataset);
+				ResultSet result = qexec.execSelect();
+				int k = 1;
+				while (result.hasNext()) {
+					QuerySolution res = result.next();
+					resource.content += "\"Triple" + k++ + "\": " + res.toString();
+					resource.content += ",";
+				}
+				dataset.end();
+			}
+			int length = (resource.content).length();
+			if(length>2)
+			resource.content = resource.content.substring(0,length-2) ;
+			resource.content += "}";
+			return resource;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		return resource;
 	}
 	
