73193a2c842f691359d625c10ef20300b5e638ac
Victor Charpenay <victor.charpenay@siemens.com>
2018-06-26 00:11:16 +0200

    refactored Groovy code; added Property defaults

--- src/main/groovy/org/eclipse/thingweb/directory/utils/TDTransform.groovy
+++ src/main/groovy/org/eclipse/thingweb/directory/utils/TDTransform.groovy
@@ -37,107 +37,156 @@ class TDTransform {
 	}
 
 	String asJsonLd10() {
-		def ctx = ["@context": ["properties": "http://www.w3.org/ns/td/schema#properties"]]
-		
-		def td = object.collectEntries({ k, v ->
-			switch (k) {
-				case "properties":
-					v = v.collect({ id, p -> ["@id": id, *:ctx, *:asJsonLd10Schema(p)] })
-					break
-				case "actions":
-					v = v.collect({ id, a ->
-						def i = asJsonLd10Schema(a."input")
-						def o = asJsonLd10Schema(a."output")
-						["@id": id, *:ctx, *:a, "input": i, "output": o]
-					})
-					break
-				case "events":
-					// TODO
-					break
-			}
-			[(k): (v)]
-		})
-		
-		if (!td."@context") {
-			td."@context" = TD_CONTEXT_URI
-		}
-		
-		if (!td."@type") {
-			td."@type" = "Thing"
-		}
-		
-		td."@id" = td."id"
-		td.remove("id")
-		
-		// TODO
-		// - add default values
-		// - context and type can be arrays
-		
+		def td = asJsonLd10ForType(object, "Thing")
 		JsonOutput.toJson(td)
 	}
 
 	String asJsonLd11() {
-		def td = object
-		
-		if (td."@graph") {
-			td = td."@graph".find()
-			td."@context" = TD_CONTEXT_URI
-		}
-		
-		td = td.collectEntries({ k, v ->
-			switch (k) {
-				case "properties":
-					v = v.collectEntries({ p -> [(p."@id"): asJsonLd11Schema(p.findAll({ it.key != "@id" })) ] })
-					break
-				case "actions":
-					v = v.collectEntries({ a ->
-						def i = asJsonLd11Schema(a."input")
-						def o = asJsonLd11Schema(a."output")
-						[(a."@id"): [*:a.findAll({ !["@id", "input", "output"].contains(it) }), "input": i, "output": o]]
-					})
-					break
-				case "events":
-					// TODO
-					break
-			}
-			[(k): (v)]
-		})
-		
-		td."id" = td."@id"
-		td.remove("@id")
-
+		def td = asJsonLd11ForType(object, "Thing")
 		JsonOutput.toJson(td)
 	}
 	
-	private def asJsonLd10Schema(obj) {
-		if (obj instanceof Map) {
-			return obj.collectEntries({ k, v ->
-				switch (k) {
-					case "properties":
-						v = v.collect({ id, s -> ["@id": id, *:asJsonLd10Schema(s)] })
-						break
-					case "items":
-						v = asJsonLd10Schema(v)
-						break
+	private Map asJsonLd10ForType(obj, String type) {
+		switch (type) {
+			case "Thing":
+				def td = obj.collectEntries({ k, v ->
+					switch (k) {
+						case "properties":
+							return ["properties": v.collect({ p -> asJsonLd10ForType(p, "Property") })]
+						case "actions":
+							return ["actions": v.collect({ a -> asJsonLd10ForType(a, "Action") })]
+						case "events":
+							return ["events": v.collect({ a -> asJsonLd10ForType(a, "Event") })]
+						case "id":
+							return ["@id": v]
+						default:
+							return [(k): (v)]
+					}
+				})
+		
+				if (!td."@context") {
+					td."@context" = TD_CONTEXT_URI
 				}
-				[(k): (v)]
-			})
+				
+				if (!td."@type") {
+					td."@type" = "Thing"
+				}
+				
+				// TODO
+				// - add default values
+				// - context and type can be arrays
+				return td
+				
+			case "Property":
+				def id = obj.key
+				def p = obj.value
+				if (!p."writable") p."writable" 
+				return [
+					"@id": id,
+					*:asJsonLd10ForType(p, "Schema"),
+					"writable": p."writable" ?: false,
+					"observable": p."observable" ?: false
+				]
+			
+			case "Action":
+				def id = obj.key
+				def a = obj.value
+				def i = a."input"
+				def o = a."output"
+				if (i) a."input" = asJsonLd10ForType(i, "Schema")
+				if (o) a."input" = asJsonLd10ForType(o, "Schema")
+				return ["@id": id, *:a]
+			
+			case "Event":
+				def id = obj.key
+				def e = obj.value
+				return ["@id": id, *:e] // TODO
+
+			case "Schema":
+				return obj.collectEntries({ k, v ->
+					switch (k) {
+						case "properties":
+							return [
+								"http://www.w3.org/ns/td/schema#properties": v.collect({ id, s ->
+									["@id": id, *:asJsonLd10ForType(s, "Schema")]
+								})
+							]
+						case "items":
+							return ["items": asJsonLd10ForType(v, "Schema")]
+						default:
+							return [(k): (v)]
+					}
+				})
+			
+			default:
+				return obj
 		}
 	}
 	
-	private def asJsonLd11Schema(obj) {
-		if (obj instanceof Map) {
-			return obj.collectEntries({ k, v ->
-				switch (k) {
-					case "http://www.w3.org/ns/td/schema#properties":
-						k = "properties"
-						v = v.collectEntries({ s -> [(s."@id"): asJsonLd11Schema(s.findAll({ it.key != "@id" }))] })
-					case "items":
-						v = asJsonLd11Schema(v)
-						break
+	private Map asJsonLd11ForType(obj, String type) {
+		switch (type) {
+			case "Thing":
+				def td = obj
+				
+				if (td."@graph") {
+					td = td."@graph".find()
+					td."@context" = TD_CONTEXT_URI
 				}
-				[(k): (v)]
-			})
+				
+				td = td.collectEntries({ k, v ->
+					switch (k) {
+						case "properties":
+							return ["properties": v.collectEntries({ p -> asJsonLd11ForType(p, "Property")})]
+						case "actions":
+							return ["actions": v.collectEntries({ a -> asJsonLd11ForType(a, "Action")})]
+						case "events":
+							return ["events": v.collectEntries({ e -> asJsonLd11ForType(e, "Event")})]
+						case "@id":
+							return ["id": v]
+						default:
+							return [(k): (v)]
+					}
+				})
+				
+				return td
+			
+			case "Property":
+				def id = obj."@id"
+				def p = obj.findAll({ it.key != "@id" })
+				return [(id): asJsonLd11ForType(p, "Schema")]
+			
+			case "Action":
+				def id = obj."@id"
+				def a = obj.findAll({ it.key != "@id" })
+				def i = a."input"
+				def o = a."output"
+				if (i) a."input" = asJsonLd11ForType(i, "Schema")
+				if (o) a."output" = asJsonLd11ForType(o, "Schema")
+				return [(id): a]
+			
+			case "Event":
+				def id = obj."@id"
+				def e = obj.findAll({ it.key != "@id" })
+				return [(id): e] // TODO
+			
+			case "Schema":
+				return obj.collectEntries({ k, v ->
+					switch (k) {
+						case "http://www.w3.org/ns/td/schema#properties":
+							return [
+								"properties": v.collectEntries({ s ->
+									[(s."@id"): asJsonLd11ForType(s.findAll({ it.key != "@id" }), "Schema")]
+								})
+							]
+						case "items":
+							return ["items": asJsonLd11ForType(v, "Schema")]
+						default:
+							return [(k): (v)]
+					}
+				})
+			
+			default:
+				return obj
 		}
 	}
 
