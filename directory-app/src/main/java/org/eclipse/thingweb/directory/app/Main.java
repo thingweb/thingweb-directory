6adb7227030889c84ce58dfe2aecf686a444ed85
Victor Charpenay <victor.charpenay@siemens.com>
2018-08-27 17:26:30 +0200

    addded CLI arguments (code taken from master)

--- directory-app/src/main/java/org/eclipse/thingweb/directory/app/Main.java
+++ directory-app/src/main/java/org/eclipse/thingweb/directory/app/Main.java
@@ -14,31 +14,66 @@
  ********************************************************************************/
 package org.eclipse.thingweb.directory.app;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.resource.Resource;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Main {
+	
+	public static final Integer DEFAULT_HTTP_PORT = 8080;
 
-	public static void main(String[] args) throws Exception {
-		Server server = new Server(8080);
+	public static void main(String[] args) throws Exception {		
+		Options options = new Options();
+		options.addOption("p", true, "HTTP port number. Default is 8080.");
+		options.addOption("h", false, "This help message.");
+
+		CommandLineParser parser = new DefaultParser();
+		CommandLine cmd = parser.parse(options, args);
+
+		if (cmd.hasOption("h")) {
+		    HelpFormatter formatter = new HelpFormatter();
+		    formatter.printHelp("thingweb-directory", options);
+			System.exit(0);
+		}
 		
-		Resource base = Resource.newClassPathResource("webapp");
-		Resource desc = base.addPath("WEB-INF/web.xml");
+		// TODO add SPARQL endpoint options
+
+		int port = DEFAULT_HTTP_PORT;
+		if (cmd.hasOption("p")) {
+			port = Integer.parseInt(cmd.getOptionValue("p"));
+		}
 		
-		WebAppContext ctx = new WebAppContext();
-		ctx.setContextPath("/");
-		ctx.setBaseResource(base);
-		ctx.setDescriptor(desc.toString());
-		
-		server.setHandler(ctx);
+		Server server = initServer(port);
 		
 		server.start();
-		
+		// TODO use logger instead
 		server.dumpStdErr();
-		
 		// TODO signal to stop server
 		server.join();
+	}
+	
+	private static Server initServer(Integer port) throws Exception {
+		try {
+			Resource base = Resource.newClassPathResource("webapp");
+			Resource desc = base.addPath("WEB-INF/web.xml");
+			
+			WebAppContext ctx = new WebAppContext();
+			ctx.setContextPath("/");
+			ctx.setBaseResource(base);
+			ctx.setDescriptor(desc.toString());
+
+			Server server = new Server(port);
+			server.setHandler(ctx);
+			
+			return server;
+		} catch (Exception e) {
+			throw new RuntimeException("Cannot find or open web.xml descriptor file.", e);
+		}
 	}
 	
 }
